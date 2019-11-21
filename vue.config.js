48b875652923e585c5c77cc4fb27de44d2440605
misato.uematsu <misato.uematsu@gpol.co.jp>
2019-11-21 18:58:46 +0900

    [modify] プリレンダリング

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
+
+
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+//const items = itemas
+
+const dynamicRoutes  = ['/item/1','/item/2','/item/3','/item/4']
+const staticRoutes = ['/', '/about', ]
+const allRoutes = staticRoutes.concat(dynamicRoutes);
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: () => {
+    return {
+        plugins: [
+            new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, 'dist'),
+                routes: allRoutes
+            })
+        ]
+    }
+  }
 }
